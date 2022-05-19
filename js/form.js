1f3fc3669f4821016a7afe60b865f954d73df531
Marcos Lopez <marcosglopez07@gmail.com>
2022-05-19 11:26:05 -0300

    añadiendo 2da opcion

--- js/form.js
+++ js/form.js
@@ -26,7 +26,7 @@ formulario.addEventListener('submit', function(e) {
             method: "POST",
             body: JSON.stringify({
                 tittle: "DatosUser",
-                body: new FormData(e.currentTarget),
+                body: data
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -37,3 +37,42 @@ formulario.addEventListener('submit', function(e) {
 
 
 })
+
+
+
+/* function verificar(e) {
+    //Previene que se reinicie el sitio al presionar Enviar
+    e.preventDefault();
+
+    let nombre = document.getElementById('nombre').value;
+    let edad = document.getElementById('edad').value;
+    let email = document.getElementById('email').value;
+
+    //Devuelve error si no se ingresa lo requerido
+    if (nombre === '' || edad === '' || email === '') {
+        swal.fire({
+            type: 'error',
+            tittle: 'ups',
+            text: 'Ingrese todos los campos requeridos :)',
+            showConfirmButton: false,
+            timer: 100
+        })
+    } else {
+        users.showUsers(nombre, edad, email)
+    }
+
+    let data = new FormData(document.getElementById("formulario"));
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify({
+                tittle: "DatosUser",
+                body: data
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(json))
+}*/
